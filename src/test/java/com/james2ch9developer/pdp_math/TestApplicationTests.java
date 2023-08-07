2637dae6ef53d7970ff1517189882faa995a2c92
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2023-08-06 20:35:09 -0500

    Initial commit. Verifying versions.
    Verifying:
      Springframework 6.x.x
      Java JDK 17.x.x
      Java version 17

---
+++ src/test/java/com/james2ch9developer/pdp_math/TestApplicationTests.java
@@ -0,0 +1,26 @@
+package com.james2ch9developer.pdp_math;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.system.JavaVersion;
+import org.springframework.boot.system.SystemProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.SpringVersion;
+import org.springframework.test.annotation.DirtiesContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@DirtiesContext
+class TestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+      // Verifies the spring framework for version.
+      assertEquals("6.0.10", SpringVersion.getVersion());
+      // VerifiesJDK version.
+      assertEquals("17.0.8", SystemProperties.get("java.version"));
+      // Verifies Java version.
+      assertEquals("17", JavaVersion.getJavaVersion().toString());
+    }
+
+}
