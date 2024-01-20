30469826e2319eb12107e3d5bd4658bbec3af94d
James Hawley II <iliketofeelcheesetootoo@yahoo.com>
2024-01-19 21:56:09 -0600

    Revert and/or comment out stuff.
    This will be my first commit since Christmas break; first commit of 2024 for this project. Revert and/or a common stuff out. Update versions based on dependabot recommendations.

--- src/main/java/com/james2ch9developer/pdp_math/PdpMathApplication.java
+++ src/main/java/com/james2ch9developer/pdp_math/PdpMathApplication.java
@@ -1,9 +1,19 @@
 package com.james2ch9developer.pdp_math;
 
+//import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+//import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+//import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@SecurityScheme(
+//  name = "Keycloak",
+//  openIdConnectUrl = "http://localhost:8081/realms/master/.well-known/openid-configuration",
+//  scheme = "bearer",
+//  type = SecuritySchemeType.OPENIDCONNECT,
+//  in = SecuritySchemeIn.HEADER
+//)
 public class PdpMathApplication {
 
   public static void main(final String[] args) {
--- src/main/java/com/james2ch9developer/pdp_math/configuration/SecurityConfig.java
+++ src/main/java/com/james2ch9developer/pdp_math/configuration/SecurityConfig.java
@@ -1,20 +1,20 @@
-package com.james2ch9developer.pdp_math.configuration;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-public  class SecurityConfig {
-
-  @Bean
-  public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
-    http.csrf(csrf -> csrf.disable())
-      .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated()
-      ).httpBasic(Customizer.withDefaults());
-
-    return http.build();
-  }
-}
+//package com.james2ch9developer.pdp_math.configuration;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//public  class SecurityConfig {
+//
+//  @Bean
+//  public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
+//    http.csrf(csrf -> csrf.disable())
+//      .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated()
+//      ).httpBasic(Customizer.withDefaults());
+//
+//    return http.build();
+//  }
+//}
