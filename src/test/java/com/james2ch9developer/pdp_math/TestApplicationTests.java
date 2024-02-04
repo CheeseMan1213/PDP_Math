37a77bee811410b41d9c33caf36c346c836f72ea
James Hawley II <iliketofeelcheesetootoo@yahoo.com>
2024-02-04 17:51:00 -0600

    Fully commented out file called TestApplicationTests.java and did dependabot updates.

--- src/test/java/com/james2ch9developer/pdp_math/TestApplicationTests.java
+++ src/test/java/com/james2ch9developer/pdp_math/TestApplicationTests.java
@@ -1,28 +1,27 @@
-package com.james2ch9developer.pdp_math;
-
-import org.junit.jupiter.api.Test;
+//package com.james2ch9developer.pdp_math;
+//
+//import org.junit.jupiter.api.Test;
 //import org.springframework.boot.system.JavaVersion;
-// import org.springframework.boot.system.SystemProperties;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.system.SystemProperties;
+//import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.core.SpringVersion;
-import org.springframework.test.annotation.DirtiesContext;
-
+//import org.springframework.test.annotation.DirtiesContext;
+//
 //import static org.junit.jupiter.api.Assertions.assertEquals;
-
-@SpringBootTest
-@DirtiesContext
-class TestApplicationTests {
-
-    @Test
-    public void contextLoads() {
-      // Verifies the spring framework for version.
+//
+//@SpringBootTest
+//@DirtiesContext
+//class TestApplicationTests {
+//
+//    @Test
+//    public void contextLoads() {
+//      // Verifies the spring framework for version.
 //      assertEquals("6.0.11", SpringVersion.getVersion());
-      // VerifiesJDK version.
-      //    I do not really care about the more specific version number.
-      //    Just that it is 17.
-      // assertEquals("17.0.8", SystemProperties.get("java.version"));
-      // Verifies Java version.
+//      // VerifiesJDK version.
+//      //    I do not really care about the more specific version number.
+//      //    Just that it is 17.
+//       assertEquals("17.0.8", SystemProperties.get("java.version"));
+//      // Verifies Java version.
 //      assertEquals("17", JavaVersion.getJavaVersion().toString());
-    }
-
-}
+//    }
+//}
